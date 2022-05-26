d7d51306850e76c833c8ab614655621935a660e8
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2022-05-26 15:41:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
 import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
-
-# to stem words
 from nltk.stem import PorterStemmer
-
-# object/instance of class PorterStemmer()
 stemmer = PorterStemmer()
-
-# importing json lib
 import json
-
-# to store data into files
 import pickle
 
 import numpy as np
 
-words=[] #list of unique roots words in the data
-classes = [] #list of unique tags in the data
-pattern_word_tags_list = [] #list of the pair of (['words', 'of', 'the', 'sentence'], 'tags')
-
-# words to be ignored while creating Dataset
+words=[]
+classes = [] 
+pattern_word_tags_list = [] 
 ignore_words = ['?', '!',',','.', "'s", "'m"]
-
-# opening JSON file, reading data from it, then closing it.
 train_data_file = open('intents.json')
 data = json.load(train_data_file)
 train_data_file.close()
-
-# creating function to stem words
 def get_stem_words(words, ignore_words):
     stem_words = []
     for word in words:
@@ -45,20 +31,14 @@ List of sorted stem words for our dataset :
 'product', 'provid', 'see', 'sell', 'show', 'smartphon', 'tell', 'thank', 'that', 'the', 'there', 
 'till', 'time', 'to', 'trend', 'video', 'what', 'which', 'you', 'your']
 '''
-
-# creating a function to make corpus
 def create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words):
 
     for intent in data['intents']:
-
-        # Add all patterns and tags to a list
         for pattern in intent['patterns']:            
             pattern_word = nltk.word_tokenize(pattern)            
             words.extend(pattern_word)                        
             pattern_word_tags_list.append((pattern_word, intent['tag']))
               
-    
-        # Add all tags to the classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
             
@@ -69,24 +49,14 @@ def create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words):
 
     return stem_words, classes, pattern_word_tags_list
 
-
-# Training Dataset: 
-# Input Text----> as Bag of Words 
-# Tags-----------> as Label
-
 def bag_of_words_encoding(stem_words, pattern_word_tags_list):
     
     bag = []
     for word_tags in pattern_word_tags_list:
-        # example: word_tags = (['hi', 'there'], 'greetings']
-
-        pattern_words = word_tags[0] # ['Hi' , 'There]
+        pattern_words = word_tags[0] 
         bag_of_words = []
 
-        # stemming pattern words before creating Bag of words
         stemmed_pattern_word = get_stem_words(pattern_words, ignore_words)
-
-        # Input data encoding 
         for word in stem_words:            
             if word in stemmed_pattern_word:              
                 bag_of_words.append(1)
@@ -102,17 +72,12 @@ def class_label_encoding(classes, pattern_word_tags_list):
     labels = []
 
     for word_tags in pattern_word_tags_list:
-
-        # Start with list of 0s
         labels_encoding = list([0]*len(classes))  
 
-        # example: word_tags = (['hi', 'there'], 'greetings']
-
-        tag = word_tags[1]   # 'greetings'
+        tag = word_tags[1]   
 
         tag_index = classes.index(tag)
 
-        # Labels Encoding
         labels_encoding[tag_index] = 1
 
         labels.append(labels_encoding)
@@ -123,7 +88,6 @@ def preprocess_train_data():
   
     stem_words, tag_classes, word_tags_list = create_bot_corpus(words, classes, pattern_word_tags_list, ignore_words)
     
-    # Convert Stem words and Classes to Python pickel file format
     pickle.dump(stem_words, open('words.pkl','wb'))
     pickle.dump(tag_classes, open('classes.pkl','wb'))
 
